98ba1cb92e80332a4e1d470b4d056cb8b569d98e
zhulin <JulinZhu@boyaa.com>
2017-03-24 18:36:34 +0800

    refact

--- utils/utils.go
+++ utils/utils.go
@@ -3,24 +3,26 @@ package utils
 import (
 	"os"
 
-	log "github.com/sirupsen/logrus"
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
 )
 
 //CheckError if err is not nil, then log the detail and return true, otherwise return false
 func CheckError(err error, desc string) (ret bool) {
+	ret = false
 	if err != nil {
 		log.Error(fmt.Sprintf("Error[%s]: %s", desc, err.Error()))
-		return true
+		ret = true
 	}
-	return false
+	return
 }
 
 //CheckFileIsExist return true when filename exists, otherwise return false
-func CheckFileIsExist(filename string) bool {
-	var exist = true
+func CheckFileIsExist(filename string) (exist bool) {
+	exist = true
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		exist = false
 	}
-	return exist
+	return
 }
